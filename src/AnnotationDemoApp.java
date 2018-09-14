2de04bf439f0b8eab893bd05dc75514061b1edbf
JMITCH18 <ronmitchell1@yahoo.com>
2018-09-13 19:21:07 -0500

    Initial commit

---
+++ src/AnnotationDemoApp.java
@@ -0,0 +1,20 @@
+import com.spring.Coach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from the spring container
+        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the container
+        context.close();
+    }
+}
